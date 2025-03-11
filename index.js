a92f036e947bee1db539dbcaf0d5acef19dbe2c5
unknown <guillermo@electriqmarketing.com>
2025-03-10 22:16:34 -0700

    feat: added animations to background

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ const express = require('express')
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
+const cookieParser = require("cookie-parser")
+const { v4: uuidv4 } = require("uuid")
+const userScrapeData = new Map()
 
 puppeteer.use(StealthPlugin())
+app.use(cookieParser());
 app.use(express.json())
 app.use(express.static('public'))
 
+app.use((req, res, next) => {
+    let userId = req.cookies.userId
+    
+    if (!userId) {
+        userId = uuidv4() // Generate a new unique ID
+        res.cookie("userId", userId, { httpOnly: true, sameSite: "Strict" })
+    }
+
+    if (!userScrapeData.has(userId)) {
+        userScrapeData.set(userId, { progress: 0, status: "Not started", total: 0 })
+    }
+
+    req.userId = userId;
+    req.scrapeData = userScrapeData.get(userId)
+    next()
+});
+
 app.get('/scrape', async (req, res) => {
+    const { scrapeData } = req
+
+    // Update user-specific progress
+    scrapeData.status = "In progress"
+    scrapeData.progress = 0
+
     try{
-        const scrapeFanCollection = async (fan) => {
+        const scrapeFanCollection = async (fan, page) => {
             const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
             await page.setUserAgent(randomUserAgent);
 
@@ -21,7 +49,7 @@ app.get('/scrape', async (req, res) => {
                 // Hide page footer
                 const modal = document.querySelector("page-footer")
                 if (modal) {
-                modal.style.display = 'none'; // Hide the modal
+                    modal.style.display = 'none'; // Hide the modal
                 }
             });
 
@@ -44,16 +72,16 @@ app.get('/scrape', async (req, res) => {
                 
                     // Incrementally scroll down the page
                     await page.evaluate(() => {
-                        window.scrollBy(0, 10000); // Scroll 10000px downwards
-                    });
+                        window.scrollBy(0, 5000); // Scroll 5000px downwards
+                    })
                 
                     // Wait for content to load
                     await new Promise(r => setTimeout(r, 1000)); // Adjust this as needed for content load time
                 
                     // Get the new height of the .collection-grid element after scrolling
                     currentHeight = await page.evaluate(() => {
-                    const grid = document.querySelector('.collection-grid');
-                    return grid ? grid.scrollHeight : 0;
+                        const grid = document.querySelector('.collection-grid');
+                        return grid ? grid.scrollHeight : 0;
                     });
                 }
             };
@@ -70,12 +98,10 @@ app.get('/scrape', async (req, res) => {
                     {},  // options
                     expandContainerSelector // selector passed here
                 )
-                .then(() => console.log("Click successful! 'show-button' class removed."))
-                .catch(() => console.log("Click might have failed or class didn't change."));
 
                 await scrollToBottom()
             } else {
-                console.log("Button not found for user " + fan.name);
+                //console.log("Button not found for user " + fan.name);
             }
 
             // Scrape the user data
@@ -110,11 +136,55 @@ app.get('/scrape', async (req, res) => {
                 });
             });
 
+            await page.close()
+
             return fansData
         };
 
-        let allCollections = []
+        const processBatch = async (batch, scraped) => {
+            const inProgress = []  // Keeps track of ongoing promises
 
+            // Split the fansData into batches of size MAX_CONCURRENCY_LIMIT (5)
+            for (let i = 0; i < batch.length; i++) {
+                const fan = batch[i];
+                // Create a promise for this fan scraping task
+                const task = (async () => {
+                    try {
+                        const fanPage = await browser.newPage()
+                        const data = await scrapeFanCollection(fan, fanPage)
+                        await fanPage.close()
+
+                        scraped.push(fan)
+                        scrapeData.progress = scraped.length
+
+                        return data;
+
+                    } catch (error) {
+                        return null
+                    }
+                })();
+
+                inProgress.push(task)
+            }
+
+            return Promise.all(inProgress)
+        }
+        
+        const processInBatches = async (fansData) => {
+            const results = []
+            const scraped = []
+
+            scrapeData.total = fansData.length
+            // Process the fansData in batches of 5
+            for (let i = 0; i < fansData.length; i += MAX_CONCURRENCY_LIMIT) {
+                const batch = fansData.slice(i, i + MAX_CONCURRENCY_LIMIT);  // Slice the data into batches of 5
+                const batchResults = await processBatch(batch, scraped);  // Process the current batch
+                results.push(...batchResults);  // Collect the batch promises
+            }
+
+            return results
+        }
+        
         const userAgents = [
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
@@ -124,17 +194,16 @@ app.get('/scrape', async (req, res) => {
         // Launch browser
         const browser = await puppeteer.launch({
             headless: false, // Set to 'false' if you want to see the browser interact
+            args: ['--no-sandbox', '--disable-setuid-sandbox'] // For better performance
         });
 
-        const page = await browser.newPage(); // Create a new page
+        const MAX_CONCURRENCY_LIMIT = 3
         const trackUrl = req.query.url
 
-        const fansData = await scrapeFans(trackUrl)
+        const page = await browser.newPage(); // Create a new page
+        const fansData = await scrapeFans(trackUrl) // Scrapes fans of givem track
 
-        for (const fan of fansData) {
-            const data = await scrapeFanCollection(fan);
-            allCollections.push(data)
-        }
+        let allCollections = await processInBatches(fansData)
 
         const trackCountMap = new Map()
 
@@ -162,16 +231,49 @@ app.get('/scrape', async (req, res) => {
 
         // Sorting by count in descending order (most frequent first)
         const sortedTracks = tracksWithCount.sort((a, b) => b.count - a.count);
-        const top10 = sortedTracks.slice(0, 10)
+        const top10 = sortedTracks.slice(1, 10)
 
-        res.send(JSON.stringify(top10))
+        res.send(JSON.stringify(sortedTracks))
     }
     catch (error) {
         console.log(error)
     }
 })
 
+app.get('/scrape-progress', async (req, res) => {
+    const { scrapeData } = req
+
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const interval = setInterval(() => {
+        const progressData = {
+            progress: scrapeData.progress,
+            total: scrapeData.total
+        };
+        
+        res.write(`data: ${JSON.stringify(progressData)}\n\n`);
+
+        if (scrapeData.progress >= scrapeData.total) {
+            clearInterval(interval);
+            res.end();
+        }
+    }, 1000);
+
+    req.on('close', () => clearInterval(interval)); // Cleanup on disconnect
+})
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+});
+
+// Gracefully close the server when nodemon restarts
+process.on("SIGTERM", () => {
+    console.log("Closing server...");
+    server.close(() => {
+        console.log("Server closed.");
+        process.exit(0);
+    });
 });
